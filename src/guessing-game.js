05aca0daa2a91963a3b9801fa68b9fce5e735af0
skysharkk <sky.light.blr@gmail.com>
2019-10-09 14:04:20 +0300

    feat: done task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.result = null;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.result = Math.round((this.max - this.min) / 2);
+        return this.result;
     }
 
     lower() {
-
+        this.max = this.result;
     }
 
     greater() {
-
+        this.min = - this.result;
     }
 }
 
